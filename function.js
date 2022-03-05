1dc8c4a2ee2d9f81c398f11e23eaea2f650cad3f
shourya12 <shouryasrivastava7@gmail.com>
2022-03-05 22:45:34 +0530

    reason for function in Js called first class citizen

---
+++ function.js
@@ -0,0 +1,34 @@
+//normal function
+function sayHello()
+{
+    console.log("hi from function");
+}
+
+sayHello();
+
+//function with argument
+function sum (num1,num2)
+{
+    let sum=num1+num2;
+    console.log("sum = "+sum)
+}
+
+sum(10,20);
+
+//function with return type
+
+function multiply(num1,num2)
+{
+    return num1*num2;
+}
+
+let ans=multiply(10,40);
+console.log("multiplication = "+ans);
+
+//Storing a function in variable ---->> *****That's why function are called --"First class citizen in javaScript"
+
+let a=function div(num1,num2){
+    return num1/num2;
+}
+
+console.log("Division = "+a(10,2));
