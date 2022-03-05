5f752900d9948cf4c6a57379d20231abb367756b
shourya12 <shouryasrivastava7@gmail.com>
2022-03-05 23:45:28 +0530

    replace method

--- string.js
+++ string.js
@@ -13,8 +13,7 @@ let slicestr=s.slice(2,5);
 console.log(slicestr);
 
 
-let arr=[1,2,3,4,5,6,7,8,9]
+//replace(old,new)
 
-let slicearr=arr.slice(2,5);
-
-console.log(slicearr);
+let replacestr=s.replace("Hi","Hello");
+console.log(replacestr);
