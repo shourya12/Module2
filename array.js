80cc92551157d413b70e9ca1519f773e2a42e809
shourya12 <shouryasrivastava7@gmail.com>
2022-03-05 23:17:51 +0530

    array into and push method in array

---
+++ array.js
@@ -0,0 +1,28 @@
+let arr=[];
+
+console.log(arr);
+
+//array with element
+
+let ele=[1,2,"ffdsvd",'s',false,2.3];
+console.log(ele);
+
+
+//print element at particular index
+
+console.log(ele[3]);
+
+//change some array element
+
+ele[2]="hi";
+
+console.log(ele);
+
+console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+
+//array method
+
+ele.push("push kr diya");
+
+console.log(ele);
+
