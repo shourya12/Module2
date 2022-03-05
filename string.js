0e5c8d551e5195c55a2a734259b332d080eb2fd8
shourya12 <shouryasrivastava7@gmail.com>
2022-03-05 23:42:44 +0530

    slice method

---
+++ string.js
@@ -0,0 +1,20 @@
+let s="Hi kya ho rha hai"
+
+console.log(s);
+
+
+console.log(s.length);
+
+
+//slice in string
+
+let slicestr=s.slice(2,5);
+
+console.log(slicestr);
+
+
+let arr=[1,2,3,4,5,6,7,8,9]
+
+let slicearr=arr.slice(2,5);
+
+console.log(slicearr);
